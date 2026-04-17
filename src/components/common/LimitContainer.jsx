aa16ba01248f9846a2bd2e5fb69fc975e27c7d6f
BenitoLM <benito.lopez.tecno@gmail.com>
2026-04-16 20:43:42 -0600

    add: new custom astro

---
+++ src/components/common/LimitContainer.jsx
@@ -0,0 +1,7 @@
+export default function LimitContainer({ children, className = "" }) {
+  return (
+    <div className={`w-9/10 max-w-7xl mx-auto bg-amber-400 ${className}`}>
+      {children}
+    </div>
+  );
+}
